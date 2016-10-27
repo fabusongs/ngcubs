e1c03175a508fac25fde78d7d8daa9a103f4fbe7
“Fabus” <info@touchtap.net>
2016-10-27 11:08:12 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,98 @@
+(function() {
+  'use strict';
+
+  var app = angular.module('app', []).run(function() {
+    console.log('Toggle the "Basic Mode" and "Promise Mode" buttons for messages and Objects');
+    console.log('App fired');
+  });
+
+  app.factory('cubService', [
+    '$http', '$q',
+    function cubService($http, $q) {
+      console.log('Cubs service fired');
+
+      var service = {
+        cubs: [],
+        getCubs: getCubs,
+        getCubsBasic: getCubsBasic
+      };
+      return service;
+
+      function getCubs() {
+        var def = $q.defer();
+
+        $http.get("http://www.touchtap.net/myapi/cubs10best.json")
+          .success(function(data) {
+            service.cubs = data;
+            def.resolve(data);
+            console.log('Cubs .. "basic mode $http" returned to controller.', data);
+          })
+          .error(function() {
+            def.reject("Failed to get Cubs");
+          });
+
+        return def.promise;
+      }
+
+      function getCubsBasic() {
+        return $http.get("http://www.touchtap.net/myapi/cubs10best.json")
+          .success(function(cubs) {
+            service.cubs = cubs;
+          //alert('See console log for promises kept.');
+            console.log('The 2016 Cubs win the World Series? - That\'s the "promise" in Chicago! (Unless you\'re a White Sox fan.)');
+          });
+      }
+    }
+  ]);
+
+  app.controller('cubsController', [
+    '$scope', 'cubService',
+    function cubsController($scope, cubService) {
+      console.log('Cubs controller fired');
+      var vm = this;
+      vm.cubs = [];
+      vm.basicMode = true;
+
+      vm.getCubs = function() {
+        cubService.getCubs()
+          .then(function(cubs) {
+              for (var i = 0; i < cubs.length; i++) {
+                cubs[i].cubName += "   ...   $q ";
+              }
+              vm.cubs = cubs;
+              console.log('Cubs ... "promise mode $q" returned to controller.', cubs);
+            },
+            function() {
+              console.log('Cubs call failed.');
+            });
+      };
+
+      vm.getCubsBasic = function() {
+        cubService.getCubsBasic()
+          .success(function(cubs) {
+            for (var i = 0; i < cubs.length; i++) {
+              cubs[i].cubName += "";
+            }
+            vm.cubs = cubs;
+            console.log('Cubs returned to controller.', vm.cubs);
+          })
+          .error(function(http, status, fnc, httpObj) {
+            console.log('Cubs retrieval failed.', http, status, httpObj);
+          });
+      };
+
+      vm.toggleBasicMode = function(mode) {
+        vm.basicMode = mode;
+        if (vm.basicMode)
+          vm.getCubsBasic();
+        else
+          vm.getCubs();
+      }
+
+      if (vm.basicMode)
+        vm.getCubsBasic();
+      else
+        vm.getCubs();
+    }
+  ]);
+})();
